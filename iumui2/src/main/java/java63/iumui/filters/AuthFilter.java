06df7c4b458c1b96550f19d0012572cbd2883222
HKCHO <j880825@gmail.com>
2015-02-04 12:07:04 +0900

    AuthFilter.java 활성화 - 비 로그인시 사용 불

--- iumui2/src/main/java/java63/iumui/filters/AuthFilter.java
+++ iumui2/src/main/java/java63/iumui/filters/AuthFilter.java
@@ -1,6 +1,7 @@
 package java63.iumui.filters;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,7 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class AuthFilter implements Filter {
-
+//	private String auth;
+//	private String common;
+//	private String fileupload;
+//	private String icon;
+//	private String js;
+//	private String main;
+	
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {}
 
@@ -25,20 +32,36 @@ public class AuthFilter implements Filter {
     HttpServletRequest request = (HttpServletRequest)req;
     HttpServletResponse response = (HttpServletResponse)resp;
 
-    if (!request.getServletPath().startsWith("/auth") &&
-        request.getSession().getAttribute("loginUser") == null) {
-      
-      request.getSession().setAttribute("requestUrl", 
-          request.getRequestURL() + "?" + request.getQueryString());
-      
-      response.sendRedirect(
-          request.getServletContext().getContextPath() // => /iumui
-          + "/auth/login.do");
-      return;
-      
+    System.out.println("1" + request.getRequestURI());
+    System.out.println("2" + request.getServletPath());
+    System.out.println("3" + request.getPathTranslated());
+    System.out.println("4" + request.getRequestURL());
+    
+    String path = request.getServletPath();
+    if (path.startsWith("/main") || path.startsWith("/common") ) {
+    		System.out.println("--------------필터를 통과하였음--------------");
+        nextFilter.doFilter(request, response); // Just continue chain.
     } else {
-      nextFilter.doFilter(request, response);
+        // business stuff for all paths other than /specialpath.
+    	System.out.println("--------------필터에 걸림--------------");
+    	
+      	if (!request.getServletPath().startsWith("/auth") &&
+            request.getSession().getAttribute("loginUser") == null) {
+          
+          request.getSession().setAttribute("requestUrl", 
+              request.getRequestURL() + "?" + request.getQueryString());
+          
+          request.getSession().setAttribute("IUMUImessage","로그인 후 이용해주십시오");
+          
+          response.sendRedirect("/iumui/main/main.html");
+
+          return;
+          
+        } else {
+          nextFilter.doFilter(request, response);
+        }
     }
+
   }
 
   @Override
